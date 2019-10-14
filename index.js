ec85faeaebeff0ed4f68c6a4540d7f2fb6a7d198
danRolon <daniel.rolon@yahoo.com>
2019-10-14 12:07:10 -0500

    post

--- index.js
+++ index.js
@@ -71,7 +71,10 @@ server.post('/hash', (req, res) => {
     .first()
     .then(user => {
       if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
+        if(bcrypt.compareSync(password, user.password){
+
+          res.status(200).json({ message: `Welcome ${user.username}!` });
+        }
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
